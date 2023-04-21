0ebe4088f36ade0587e0ab5a9d33f88e912b00e9
beyond88 <muhin.cse.diu@gmail.com>
2023-04-21 11:59:08 +0600

    Display income, target, percentage amount

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ function get_similiar_campaigns( $post_id ) {
 
     $related_args = array(
         'post_type' => 'give_forms',
-        'posts_per_page' => -1,
+        'posts_per_page' => 3,
         'post_status' => 'publish',
         'post__not_in' => array( $post_id ),
         'orderby' => 'rand',
@@ -89,6 +89,84 @@ function get_similiar_campaigns( $post_id ) {
 
             $img = get_the_post_thumbnail_url( get_the_ID(), 'medium' );
 
+            $form_id = get_the_ID();
+            $form        = new Give_Donate_Form($form_id);
+            $goal_option = give_get_meta($form->ID, '_give_goal_option', true);
+            
+            $goal_format         = give_get_form_goal_format($form_id);
+            $price               = give_get_meta($form_id, '_give_set_price', true);
+            $color               = give_get_meta($form_id, '_give_goal_color', true);
+            $show_text           = isset($args['show_text']) ? filter_var($args['show_text'], FILTER_VALIDATE_BOOLEAN) : true;
+            $show_bar            = isset($args['show_bar']) ? filter_var($args['show_bar'], FILTER_VALIDATE_BOOLEAN) : true;
+            $goal_progress_stats = give_goal_progress_stats($form);
+            
+            $income = $goal_progress_stats['raw_actual'];
+            $goal   = $goal_progress_stats['raw_goal'];
+            
+            switch ($goal_format) {
+            
+                case 'donation':
+                    $progress           = $goal ? round(($income / $goal) * 100, 2) : 0;
+                    $progress_bar_value = $income >= $goal ? 100 : $progress;
+                    break;
+            
+                case 'donors':
+                    $progress_bar_value = $goal ? round(($income / $goal) * 100, 2) : 0;
+                    $progress           = $progress_bar_value;
+                    break;
+            
+                case 'percentage':
+                    $progress           = $goal ? round(($income / $goal) * 100, 2) : 0;
+                    $progress_bar_value = $income >= $goal ? 100 : $progress;
+                    break;
+            
+                default:
+                    $progress           = $goal ? round(($income / $goal) * 100, 2) : 0;
+                    $progress_bar_value = $income >= $goal ? 100 : $progress;
+                    break;
+            
+            }
+            
+            /**
+             * Filter the goal progress output
+             *
+             * @since 1.8.8
+             */
+            $progress = apply_filters('give_goal_amount_funded_percentage_output', $progress, $form_id, $form);
+
+            /**
+             * Filter the give currency.
+             *
+             * @since 1.8.17
+             */
+            $form_currency = apply_filters('give_goal_form_currency', give_get_currency($form_id), $form_id);
+
+            /**
+             * Filter the income formatting arguments.
+             *
+             * @since 1.8.17
+             */
+            $income_format_args = apply_filters('give_goal_income_format_args', array(
+                'sanitize' => false,
+                'currency' => $form_currency,
+                'decimal'  => false,
+            ), $form_id);
+
+            /**
+             * Filter the goal formatting arguments.
+             *
+             * @since 1.8.17
+             */
+            $goal_format_args = apply_filters('give_goal_amount_format_args', array(
+                'sanitize' => false,
+                'currency' => $form_currency,
+                'decimal'  => false,
+            ), $form_id);
+
+            // Get formatted amount.
+            $income = give_human_format_large_amount(give_format_amount($income, $income_format_args), array('currency' => $form_currency));
+            $goal   = give_human_format_large_amount(give_format_amount($goal, $goal_format_args), array('currency' => $form_currency));
+            
             $html .='<div data-v-ae4d80ee="" data-v-cbd77764="" class="card-carousel__slide">
                 <div data-v-7f9117ba="" data-v-ae4d80ee="" class="campaign-card" data-v-cbd77764="">
                 <a data-v-7f9117ba="" href="'.get_permalink( get_the_ID() ).'" target="_self" class="campaign-card__link">
@@ -98,8 +176,8 @@ function get_similiar_campaigns( $post_id ) {
                             <i data-v-7f9117ba="" class="campaign-card__footer__owner__verified-status__icon fas fa-check"></i>
                             VERIFIED
                         </span> 
-                        <div data-v-7f9117ba="" class="campaign-card__content__title">Empower Children with Special Needs: Support MIJ Hub’s Ramadan Meal Program. [Believe in Abilities]</div>
-                        <div data-v-7f9117ba="" class="campaign-card__content__description">“Will You Help Us Give the Gift of Daily Nutritious Meals to Children in Need?" Every child deserves to have access to nutritious meals, regardless of their family\'s financial circ...</div>
+                        <div data-v-7f9117ba="" class="campaign-card__content__title">'.get_the_title().'</div>
+                        <div data-v-7f9117ba="" class="campaign-card__content__description">'.get_the_excerpt().'</div>
                     </div>
                     <div data-v-7f9117ba="" class="campaign-card__footer campaign-card__footer--with-owner">
                         <div data-v-7f9117ba="" class="campaign-card__footer__filler"></div>
@@ -109,14 +187,18 @@ function get_similiar_campaigns( $post_id ) {
                             </div>
                         </div>
                         <div data-v-7f9117ba="" class="campaign-card__footer__progress-bar">
-                            <div data-v-7f9117ba="" class="campaign-card__footer__progress-bar__progress" style="width: 7.48674%; background-color: rgb(235, 0, 140);"></div>
+                            <div data-v-7f9117ba="" class="campaign-card__footer__progress-bar__progress" style="width: '.esc_attr(round($progress)).'%; background-color: rgb(235, 0, 140);"></div>
                         </div>
                         <div data-v-7f9117ba="" class="campaign-card__footer__progress-text">
-                            <div data-v-7f9117ba="" class="campaign-card__footer__progress-text__container"><span data-v-7f9117ba="" class="campaign-card__footer__progress-text__container__progress"><span data-v-7f9117ba="">raised</span>
-                            S$4,970
-                            </span> <span data-v-7f9117ba="" class="campaign-card__footer__progress-text__container__target"><span data-v-7f9117ba="">of</span>
-                            S$66,384
-                            </span>
+                            <div data-v-7f9117ba="" class="campaign-card__footer__progress-text__container">
+                                <span data-v-7f9117ba="" class="campaign-card__footer__progress-text__container__progress">
+                                    <span data-v-7f9117ba="">raised</span>
+                                    ' . give_currency_filter($income, array('form_id' => $form_id)) . '
+                                </span> 
+                                <span data-v-7f9117ba="" class="campaign-card__footer__progress-text__container__target">
+                                <span data-v-7f9117ba="">of</span>
+                                ' . give_currency_filter($goal, array('form_id' => $form_id)) . '
+                                </span>
                             </div>
                             
                         </div>
