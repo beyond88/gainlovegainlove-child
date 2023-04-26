a49d5428c878b41ea5db4d129c4ac822a4ed803a
beyond88 <muhin.cse.diu@gmail.com>
2023-04-26 19:26:38 +0600

    Display share modal

--- assets/js/child-main.js
+++ assets/js/child-main.js
@@ -31,7 +31,6 @@ $( document ).ready(function() {
       $(".campaign-tabs__tab").removeClass("campaign-tabs__tab--selected");
       $(this).addClass("campaign-tabs__tab--selected");
       const targetTabContent = $(this).data('tab-id');
-      console.log('current tab==>',targetTabContent);
       $(".campaign-tabs__tab").each(function(index, item) {
         let currentTabContent = $(this).data('tab-id');
         if( typeof currentTabContent != "undefined" ){   
--- give/single-give-form/templates/bottom-donate.php
+++ give/single-give-form/templates/bottom-donate.php
@@ -11,13 +11,13 @@
             </a>
         </div>
     
-        <button data-v-3bb18fd3="" data-v-586aba14="" type="button" class="loading-button campaign-cta__button campaign-cta__button--orange">
+        <button data-v-3bb18fd3="" data-v-586aba14="" type="button" class="loading-button campaign-cta__button campaign-cta__button--orange gainlove-share-button" onClick="showHideContent('', '.campaign-share-dialog__lightbox')">
             <?php echo __('SHARE THIS CAMPAIGN', 'gainlove'); ?>
             <span data-v-586aba14="" data-v-3bb18fd3="" class="campaign-cta__button__count"></span>
         </button>
-        <div data-v-0ca5f774="" data-v-0d4425d0="" bis_skin_checked="1">
-            <div data-v-646760a4="" data-v-7ddbc005="" data-v-0ca5f774="" class="dialog-box js-dialogBoxWrapper" style="position: fixed; display: none;" bis_skin_checked="1">
-                <div data-v-7ddbc005="" data-v-646760a4="" class="shared-successful-dialog__container js-dialog-content" bis_skin_checked="1">
+        <div data-v-0ca5f774="" data-v-0d4425d0="">
+            <div data-v-646760a4="" data-v-7ddbc005="" data-v-0ca5f774="" class="dialog-box js-dialogBoxWrapper" style="position: fixed; display: none;">
+                <div data-v-7ddbc005="" data-v-646760a4="" class="shared-successful-dialog__container js-dialog-content">
                     <h3 data-v-7ddbc005="" data-v-646760a4="" class="shared-successful-dialog__container__header"><span data-v-7ddbc005="" data-v-646760a4="" class="shared-successful-dialog__container__header__text">
                         Successfully shared!
                         </span> <span data-v-7ddbc005="" data-v-646760a4="" class="shared-successful-dialog__container__header__text">
@@ -27,81 +27,83 @@
                     <p data-v-7ddbc005="" data-v-646760a4="" class="shared-successful-dialog__container__lead">
                         Thank you for helping to spread the word!
                     </p>
-                    <div data-v-7ddbc005="" data-v-646760a4="" class="shared-successful-dialog__container__button-container" bis_skin_checked="1">
+                    <div data-v-7ddbc005="" data-v-646760a4="" class="shared-successful-dialog__container__button-container">
                         <button data-v-3bb18fd3="" data-v-7ddbc005="" type="button" class="loading-button shared-successful-dialog__container__button-container__button-close" data-v-646760a4="">
-                        <!----> Close
+                        Close
                         </button>
                     </div>
                 </div>
             </div>
-            <div data-v-0ca5f774="" class="campaign-share-dialog__lightbox js--dismiss-el" style="display: none;" bis_skin_checked="1">
-                <div data-v-0ca5f774="" class="campaign-share-dialog__modal" bis_skin_checked="1">
-                    <button data-v-3bb18fd3="" data-v-0ca5f774="" type="button" class="loading-button campaign-share-dialog__modal__dismiss-button js--dismiss-el loading-button--flat loading-button--gray">
-                        <!----> <i data-v-0ca5f774="" data-v-3bb18fd3="" class="fal fa-times campaign-share-dialog__modal__dismiss-button__icon js--dismiss-el"></i>
+            <div data-v-0ca5f774="" class="campaign-share-dialog__lightbox js--dismiss-el" style="display: none;">
+                <div data-v-0ca5f774="" class="campaign-share-dialog__modal">
+                    <button data-v-3bb18fd3="" data-v-0ca5f774="" type="button" class="loading-button campaign-share-dialog__modal__dismiss-button js--dismiss-el loading-button--flat loading-button--gray" onClick="showHideContent('.campaign-share-dialog__lightbox', '',)">
+                        <i data-v-0ca5f774="" data-v-3bb18fd3="" class="fal fa-times campaign-share-dialog__modal__dismiss-button__icon js--dismiss-el"></i>
                     </button>
-                    <div data-v-0ca5f774="" class="campaign-share-dialog__content" bis_skin_checked="1">
-                        <div data-v-0ca5f774="" class="campaign-share-dialog__content__header" bis_skin_checked="1">
-                        A single share from you can help this campaign raise at least S$25.
+                    <div data-v-0ca5f774="" class="campaign-share-dialog__content">
+                        <div data-v-0ca5f774="" class="campaign-share-dialog__content__header">
+                            <?php echo __('A single share from you can help this campaign raise at least S$25.', 'gainlove'); ?>
                         </div>
-                        <div data-v-0ca5f774="" class="campaign-share-dialog__content__body" bis_skin_checked="1">
-                        Will you spare 10 seconds to share this campaign so that more people can help Carry Hope 2023 - Team Wenjie?
+                        <div data-v-0ca5f774="" class="campaign-share-dialog__content__body">
+                            <?php 
+                                echo __('Will you spare 10 seconds to share this campaign so that more people can help Carry Hope 2023?','gainlove');
+                            ?>
                         </div>
-                        <div data-v-0ca5f774="" class="campaign-share-dialog__content__buttons-container" bis_skin_checked="1">
-                        <div data-v-fd8b712e="" data-v-0ca5f774="" class="social-media-share" bis_skin_checked="1">
-                            <p data-v-fd8b712e="" class="social-media-share__share-text">SHARE</p>
-                            <div data-v-fd8b712e="" class="social-media-share__share-buttons" bis_skin_checked="1">
-                                <div data-v-fd8b712e="" class="social-media-share__button-container" bis_skin_checked="1">
+                        <div data-v-0ca5f774="" class="campaign-share-dialog__content__buttons-container">
+                        <div data-v-fd8b712e="" data-v-0ca5f774="" class="social-media-share">
+                            <p data-v-fd8b712e="" class="social-media-share__share-text"><?php echo __('SHARE', 'gainlove'); ?></p>
+                            <div data-v-fd8b712e="" class="social-media-share__share-buttons">
+                                <div data-v-fd8b712e="" class="social-media-share__button-container">
                                     <a data-v-fd8b712e="" href="https://give.asia/campaign/carry-hope-2023-team-wenjie/share/whatsapp?" target="_blank">
-                                    <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button whatsapp-button loading-button--flat loading-button--rounded-full loading-button--transparent">
-                                        <!----> <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-whatsapp_2x.png" alt="" class="social-media-share__button-container__button__icon">
-                                    </button>
-                                    Whatsapp
+                                        <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button whatsapp-button loading-button--flat loading-button--rounded-full loading-button--transparent">
+                                            <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-whatsapp_2x.png" alt="" class="social-media-share__button-container__button__icon">
+                                        </button>
+                                        <?php echo __('Whatsapp', 'gainlove'); ?>
                                     </a>
                                 </div>
-                                <div data-v-fd8b712e="" class="social-media-share__button-container" bis_skin_checked="1">
+                                <div data-v-fd8b712e="" class="social-media-share__button-container">
                                     <a data-v-fd8b712e="" href="https://give.asia/campaign/carry-hope-2023-team-wenjie/share/facebook?" target="_blank">
-                                    <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button facebook-button loading-button--flat loading-button--rounded-full loading-button--transparent">
-                                        <!----> <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-fb_2x.png" alt="" class="social-media-share__button-container__button__icon">
-                                    </button>
-                                    Facebook
+                                        <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button facebook-button loading-button--flat loading-button--rounded-full loading-button--transparent">
+                                            <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-fb_2x.png" alt="" class="social-media-share__button-container__button__icon">
+                                        </button>
+                                        <?php echo __('Facebook', 'gainlove'); ?>
                                     </a>
                                 </div>
-                                <div data-v-fd8b712e="" class="social-media-share__button-container" bis_skin_checked="1">
+                                <div data-v-fd8b712e="" class="social-media-share__button-container">
                                     <a data-v-fd8b712e="" href="https://give.asia/campaign/carry-hope-2023-team-wenjie/share/telegram?" target="_blank">
-                                    <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button telegram-button loading-button--flat loading-button--rounded-full loading-button--transparent">
-                                        <!----> <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-telegram_2x.png" alt="" class="social-media-share__button-container__button__icon">
-                                    </button>
-                                    Telegram
+                                        <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button telegram-button loading-button--flat loading-button--rounded-full loading-button--transparent">
+                                            <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-telegram_2x.png" alt="" class="social-media-share__button-container__button__icon">
+                                        </button>
+                                        <?php echo __('Telegram', 'gainlove'); ?>
                                     </a>
                                 </div>
-                                <div data-v-fd8b712e="" class="social-media-share__button-container" bis_skin_checked="1">
+                                <div data-v-fd8b712e="" class="social-media-share__button-container">
                                     <a data-v-fd8b712e="" href="https://give.asia/campaign/carry-hope-2023-team-wenjie/share/twitter?" target="_blank">
-                                    <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button twitter-button loading-button--flat loading-button--rounded-full loading-button--transparent">
-                                        <!----> <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-twitter_2x.png" alt="" class="social-media-share__button-container__button__icon">
-                                    </button>
-                                    Twitter
+                                        <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button twitter-button loading-button--flat loading-button--rounded-full loading-button--transparent">
+                                            <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-twitter_2x.png" alt="" class="social-media-share__button-container__button__icon">
+                                        </button>
+                                        <?php echo __('Twitter', 'gainlove'); ?>
                                     </a>
                                 </div>
-                                <div data-v-fd8b712e="" class="social-media-share__button-container" bis_skin_checked="1">
+                                <div data-v-fd8b712e="" class="social-media-share__button-container">
                                     <a data-v-fd8b712e="" href="https://give.asia/campaign/carry-hope-2023-team-wenjie/share/email?" target="_blank">
-                                    <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button email-button loading-button--flat loading-button--rounded-full loading-button--transparent">
-                                        <!----> <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-email_2x.png" alt="" class="social-media-share__button-container__button__icon">
-                                    </button>
-                                    Email
+                                        <button data-v-3bb18fd3="" data-v-fd8b712e="" type="button" class="loading-button social-media-share__button-container__button email-button loading-button--flat loading-button--rounded-full loading-button--transparent">
+                                            <img data-v-fd8b712e="" data-v-3bb18fd3="" src="https://res.cloudinary.com/dmajhtvmd/image/upload/v1664372687/assets/images/donate/share/shareBtn-email_2x.png" alt="" class="social-media-share__button-container__button__icon">
+                                        </button>
+                                        <?php echo __('Email', 'gainlove'); ?>
                                     </a>
                                 </div>
                             </div>
                         </div>
                         </div>
-                        <div data-v-0ca5f774="" class="campaign-share-dialog__content__copy-link" bis_skin_checked="1">
-                        <div data-v-46235af8="" data-v-0ca5f774="" class="input-text campaign-share-dialog__content__copy-link__url-input" readonly="readonly" type="text" bis_skin_checked="1">
+                        <div data-v-0ca5f774="" class="campaign-share-dialog__content__copy-link">
+                        <div data-v-46235af8="" data-v-0ca5f774="" class="input-text campaign-share-dialog__content__copy-link__url-input" readonly="readonly" type="text">
                             <p data-v-46235af8="" class="input-text__label" style="display: none;">
                             </p>
-                            <input data-v-46235af8="" type="text" placeholder="" step="1" readonly="readonly" class="input-text__input"> <!----> 
+                            <input data-v-46235af8="" type="text" placeholder="" step="1" readonly="readonly" class="input-text__input"> 
                             <p data-v-46235af8="" class="input-text__error" style="display: none;"></p>
                         </div>
                         <button data-v-3bb18fd3="" data-v-0ca5f774="" type="button" class="loading-button campaign-share-dialog__content__copy-link__button">
-                            <!----> <span data-v-0ca5f774="" data-v-3bb18fd3="" class="campaign-share-dialog__content__copy-link__button__label--hidden-lg">COPY</span> <span data-v-0ca5f774="" data-v-3bb18fd3="" class="campaign-share-dialog__content__copy-link__button__label--hidden-sm">COPY LINK</span>
+                            <span data-v-0ca5f774="" data-v-3bb18fd3="" class="campaign-share-dialog__content__copy-link__button__label--hidden-lg">COPY</span> <span data-v-0ca5f774="" data-v-3bb18fd3="" class="campaign-share-dialog__content__copy-link__button__label--hidden-sm">COPY LINK</span>
                         </button>
                         </div>
                     </div>
