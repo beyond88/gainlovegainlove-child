2ee028f3ea3ef17f3f0259038d6c007488ceb3af
beyond88 <muhin.cse.diu@gmail.com>
2023-04-26 08:15:54 +0600

    Tab bottom border color update

--- give/single-give-form/templates/tabs.php
+++ give/single-give-form/templates/tabs.php
@@ -1,21 +1,21 @@
 <div data-v-41aa2c30="" data-v-1d3087a2="" class="campaign-tabs campaign-nav__fixed-tabs js-fixedNavTabs">
     <div data-v-41aa2c30="" class="campaign-tabs__outer">
         <div data-v-41aa2c30="" class="campaign-tabs__inner">
-            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-story" class="campaign-tabs__tab campaign-tabs__tab--selected" style="color: rgb(235, 0, 140); border-bottom-color: rgb(235, 0, 140);">
+            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-story" class="campaign-tabs__tab campaign-tabs__tab--selected">
                 <?php echo __('Story', 'gainlove'); ?>
             </a> 
-            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-updates" class="campaign-tabs__tab campaign-tabs__tab--updates campaign-tabs__tab--unselected" style="color: rgb(235, 0, 140); border-bottom-color: rgb(235, 0, 140);">
+            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-updates" class="campaign-tabs__tab campaign-tabs__tab--updates campaign-tabs__tab--unselected">
                 <?php echo __('Updates', 'gainlove'); ?>
                 <span data-v-41aa2c30="" class="campaign-tabs__tab__count campaign-tabs__tab__count--unselected" style="background-color: rgb(235, 0, 140);">6</span>
             </a> 
-            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-activities" class="campaign-tabs__tab campaign-tabs__tab--activities campaign-tabs__tab--unselected" style="color: rgb(235, 0, 140); border-bottom-color: rgb(235, 0, 140);">
+            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-activities" class="campaign-tabs__tab campaign-tabs__tab--activities campaign-tabs__tab--unselected">
                 <?php echo __('Activities', 'gainlove'); ?>
                 <span data-v-41aa2c30="" class="campaign-tabs__tab__count campaign-tabs__tab__count--unselected" style="background-color: rgb(235, 0, 140);">1.8k</span>
             </a> 
-            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-testimonials" class="campaign-tabs__tab campaign-tabs__tab--testimonials campaign-tabs__tab--unselected" style="color: rgb(235, 0, 140); border-bottom-color: rgb(235, 0, 140);">
+            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-testimonials" class="campaign-tabs__tab campaign-tabs__tab--testimonials campaign-tabs__tab--unselected">
                 <?php echo __('Testimonials', 'gainlove'); ?>
             </a> 
-            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-top-donors" class="campaign-tabs__tab campaign-tabs__tab--top-donors campaign-tabs__tab--unselected" style="color: rgb(235, 0, 140); border-bottom-color: rgb(235, 0, 140);">
+            <a data-v-41aa2c30="" href="javascript:void(0);" data-tab-id="campaign-top-donors" class="campaign-tabs__tab campaign-tabs__tab--top-donors campaign-tabs__tab--unselected">
                 <?php echo __('Top Donors', 'gainlove'); ?>
             </a>
         </div>
